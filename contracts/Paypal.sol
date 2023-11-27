23b4e2de56646af5c2107d8c85f7cd780ede7af3
disheng <ongdisheng@gmail.com>
2023-11-28 06:19:33 +0800

    Added functions

--- contracts/Paypal.sol
+++ contracts/Paypal.sol
@@ -30,12 +30,87 @@ contract Paypal {
         bool hasName;
     }
 
-    mapping(address => User) users;
-    mapping(address => Request[]) requests;
-    mapping(address => Transaction[]) history;
+    mapping(address => User) users;             // store name for each user
+    mapping(address => Request[]) requests;     // store payment requests for each user
+    mapping(address => Transaction[]) history;  // store transaction history for each user
 
     // set owner as deployer of smart contract
     constructor() {
         owner = msg.sender;
+    }
+
+    // allow user to add name representing their address
+    function addName(string memory _name) public {
+        User storage user = users[msg.sender];
+        user.name = _name;
+        user.hasName = true;
+    }
+
+    // allow user to request coins from others
+    function createRequest(address user, uint _amount, string memory _message) public {
+        Request memory request;
+        request.requestor = msg.sender;
+        request.amount = _amount;
+        request.message = _message;
+        if (users[msg.sender].hasName) {
+            request.name = users[msg.sender].name;
+        }
+
+        // assign newly created request to respondent
+        requests[user].push(request);
+    }
+
+    // allow user to pay for request
+    function payRequest(uint requestIdx) public payable {
+        require(requestIdx < requests[msg.sender].length, "No such request");
+        Request[] storage myRequests = requests[msg.sender];
+        Request storage payableRequest = myRequests[requestIdx];
+
+        // transfer coin to requestor
+        uint toPay = payableRequest.amount * 1000000000000000000;
+        require(msg.value == toPay, "Incorrect amount");
+        payable(payableRequest.requestor).transfer(msg.value);
+
+        // remove executed request
+        addHistory(msg.sender, payableRequest.requestor, payableRequest.amount, payableRequest.message);
+        myRequests[requestIdx] = myRequests[myRequests.length - 1];
+        myRequests.pop()
+    }
+
+    // keep track of transaction history
+    function addHistory(address sender, address receiver, uint _amount, string memory _message) private {
+        Transaction memory send;
+        send.action = "-";
+        send.amount = _amount;
+        send.message = _message;
+        send.otherPartyAddress = receiver;
+        if (users[receiver].hasName) {
+            send.otherPartyName = users[receiver].name;
+        }
+        history[sender].push(send);
+
+        Transaction memory receive;
+        receive.action = "+";
+        receive.amount = _amount;
+        receive.message = _message;
+        receive.otherPartyAddress = sender;
+        if (users[sender].hasName) {
+            receive.otherPartyName = users[sender].name;
+        }
+        history[receiver].push(receive);
+    }
+
+    // retrieve all requests sent to a user
+    function getRequests(address user) public view returns(Request[] memory) {
+        return requests[user];
+    }
+
+    // retrieve historic transactions user has been apart of
+    function getHistory(address user) public view returns(Transaction[] memory) {
+        return history[user];
+    }
+
+    function getName(address user) public view returns(User memory) {
+        return users[user];
     }
 }
