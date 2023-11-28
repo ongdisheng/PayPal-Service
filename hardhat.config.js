de4ef2fa701b5cb775b6c13989861aa3dceada72
disheng <ongdisheng@gmail.com>
2023-11-29 06:05:39 +0800

    Updated config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
 };
